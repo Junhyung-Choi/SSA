90bef7383a9eb2b13461db1a2230806cbea6a48e
Junhyung-Choi <junhyung9571@naver.com>
2021-12-04 04:55:05 +0900

    UPDATE Code - exceptinal case still exist

--- algostack.py
+++ algostack.py
@@ -9,12 +9,44 @@ SIZE = 25
 # ]
 
 graph = [
-    [ 0, 0, 0, 1, 1],
-    [ 0,-1,-1, 1, 1], 
     [ 0,-1,-1,-1,-1],
-    [ 0,-1,-1,-1,-1],
-    [ 0, 0, 0, 0, 0]
+    [ 0,-1,-1,-1,-1], 
+    [ 1, 1, 1, 1, 1],
+    [ 1,-1,-1,-1,-1],
+    [ 1, 1, 1, 1, 1]
 ]
+
+# graph = [
+#     [ 0, 0, 0, 1, 1],
+#     [ 0, 0,-1, 1, 1], 
+#     [ 0, 0,-1,-1,-1],
+#     [ 0, 0, 0, 0, 0],
+#     [ 0, 0, 0, 0, 0]
+# ]
+
+# graph = [
+#     [ 0,-1, 0, 1, 1],
+#     [ 0,-1,-1, 1, 1], 
+#     [ 0,-1,-1,-1,-1],
+#     [ 0,-1,-1,-1,-1],
+#     [ 0, 0, 0, 0, 0]
+# ]
+
+# graph = [
+#     [ 0, 1, 1, 1, 1],
+#     [ 0, 1, 1, 1, 1], 
+#     [ 0, 1, 1, 1, 1],
+#     [ 0, 1, 1, 1, 1],
+#     [ 0, 0, 0, 0, 0]
+# ]
+
+# graph = [
+#     [ 0, 0, 0, 1, 0],
+#     [ 0, 1, 0, 0, 0], 
+#     [ 1, 0, 0,-1, 0],
+#     [ 1, 0,-1, 1, 0],
+#     [ 1, 0, 0,-1, 0]
+# ]
 
 spMatrix = [[[-1] for _ in range(5)] for _ in range(5)]
 
@@ -52,11 +84,13 @@ def evalStack(stack):
     if (stack[0] == -1):
         return -INF
     sx,sy = itoc(stack[0])
-    # if graph[sx][sy] == 1:
-    #     score += 5
-    #     visited_red
+    score = 0
     visited_red = [0] * 25
-    score = graph[sx][sy]
+    if graph[sx][sy] == 1:
+        score += 5
+        visited_red[stack[0]] = True
+    elif graph[sx][sy] == -1:
+        score -= 5
     for i in range(1,len(stack)):
         score -= 1
         x,y = itoc(stack[i])
@@ -74,18 +108,25 @@ def reroll(origin):
     dx = [1,-1,0,0]
     dy = [0,0,-1,1]
     origin_x, origin_y = itoc(origin)
-    ret = -1
+    ret = False
     for i in range(4):
         near_x = origin_x + dx[i]
         near_y = origin_y + dy[i]
         if (0 <= near_x <= 4 and 0 <= near_y <= 4):
             print("[%d][%d] -> [%d][%d]:"%(origin_x ,origin_y, near_x, near_y),
                    itocList(spMatrix[near_x][near_y]), itocList(spMatrix[origin_x][origin_y] + [ctoi(near_x,near_y)]))
+            print("\n")
             if evalStack(spMatrix[near_x][near_y]) <= evalStack(spMatrix[origin_x][origin_y] + [ctoi(near_x,near_y)]):
                 spMatrix[near_x][near_y] = spMatrix[origin_x][origin_y] + [ctoi(near_x,near_y)]
                 if (visited[ctoi(near_x,near_y)]):
-                    ret = ctoi(near_x,near_y)
-                visited[ctoi(near_x,near_y)] = False
+                    print("[%d][%d] has been changed. need to reroll [%d][%d]" % (near_x,near_y,near_x,near_y))
+                    # ret = ctoi(near_x,near_y)
+                    ret = True
+                    visited[ctoi(near_x,near_y)] = False
+            # if spMatrix[near_x][near_y] == [24,19,14,13]:
+            #     spMatrix[near_x][near_y] = [24,19,18,14,13]
+            # elif evalStack(spMatrix[near_x][near_y]) == evalStack(spMatrix[origin_x][origin_y] + [ctoi(near_x,near_y)]):
+            #     spMatrix[near_x][near_y] = spMatrix[origin_x][origin_y] + [ctoi(near_x,near_y)]
     visited[origin] = True
     return ret
 
@@ -93,13 +134,39 @@ def calSP(start, end):
     sx, sy = itoc(start)
     spMatrix[sx][sy][0] = start
     showGraph(spMatrix)
-    i = 0
-    while i < SIZE - 1:
-        next_check = reroll(SIZE -1 - i)
-        if (next_check == -1):
-            i += 1
+    order = [
+                (4,4),
+                (4,3),(3,4),
+                (4,2),(3,3),(2,4),
+                (4,1),(3,2),(2,3),(1,4),
+                (4,0),(3,1),(2,2),(1,3),(0,4),
+                (3,0),(2,1),(1,2),(0,3),
+                (2,0),(1,1),(0,2),
+                (1,0),(0,1),
+                (0,0)
+            ]
+    index = 0
+    while index < SIZE:
+        x,y = order[index]
+        if (visited[ctoi(x,y)]):
+            index += 1
         else:
-            i = SIZE - 1 - next_check
+            next_check = reroll(ctoi(x,y))
+            if (next_check):
+                index = 0
+            # else:
+            #     index += 1
+    # i = 0
+    # while i < SIZE - 1:
+    #     if (visited[SIZE - 1 - i]):
+    #         i += 1
+    #     else:
+    #         next_check = reroll(SIZE -1 - i)
+    #         if (next_check == -1):
+    #             i += 1
+    #         else:
+    #             # i = SIZE - 1 - next_check
+    #             i = 0
     end_x,end_y = itoc(end)
     print("End's score is :",evalStack(spMatrix[end_x][end_y]))
     print("End's route is ",spMatrix[end_x][end_y])
